d0e07eace66b36ece983191fbbb1b73793401c2a
Aaron John Groeneveld <aaron.groeneveld@gmail.com>
2018-03-05 23:56:10 -0700

    Username Support

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var people =[];
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -9,8 +11,13 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', people[socket.id]+": "+msg);
   });
+  socket.on('join', function(){
+      console.log(people);
+      people[socket.id]="User"+Object.keys(people).length;
+
+    });
 });
 
 http.listen(port, function(){
