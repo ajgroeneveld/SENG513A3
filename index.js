c49924c03e152346e37d1e9839665964c9f37fe3
Aaron John Groeneveld <aaron.groeneveld@gmail.com>
2018-03-07 19:13:55 -0700

    Some cosmetic improvements, Active users, Connect/disconnect messages, JSON format for messages

--- index.js
+++ index.js
@@ -11,15 +11,40 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', people[socket.id]+": "+msg);
+      console.log(msg);
+      var messageObj='{'+'"uname":"'+people[socket.id]+'", "message":"'+msg+'","timestamp":"'+Date.now()+'"}';
+      console.log(messageObj);
+    io.emit('chat message', messageObj);
+
   });
   socket.on('join', function(){
-      console.log(people);
-      people[socket.id]="User"+Object.keys(people).length;
-
+      console.log("user joined");
+      people[socket.id]="User"+parseInt(Object.keys(people).length+1);
+      UpdatePeople(people);
+      io.emit('user connect', people[socket.id]);
     });
+
+
+  socket.on('disconnect', function(){
+      console.log('user disconnected');
+      io.emit('user left', people[socket.id]);
+      delete people[socket.id];
+      UpdatePeople(people);
+    });
+
 });
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+
+function UpdatePeople(people){
+    var people_emit="";
+    for(i in people) {
+        if (people_emit===""){people_emit=people[i];}
+        else{people_emit=people_emit+","+people[i];}
+    }
+    io.emit('users update', people_emit);
+
+}
