b42c2eb6bc92ff4fef173cb4e9c36e45f0a46311
Aaron John Groeneveld <aaron.groeneveld@gmail.com>
2018-03-09 18:52:37 -0700

    Made things a little nicer looking

--- Public/client.js
+++ Public/client.js
@@ -1,38 +1,37 @@
-
 //We want to save username and color locally
 var username;
 var color;
 
 //When we load
-$(function () {
-   // console.log("Hello?");
+$(function() {
+    // console.log("Hello?");
     var socket = io(); //Start the socket
     socket.emit('join'); //Set up on the server
     var cookie = document.cookie.split("="); //Check if we previously had a Username
-    if (cookie[0]==="username"){    //If we did, set the username to the old one
-        socket.emit('chat message', "/nick "+cookie[1]) //I know
+    if (cookie[0] === "username") { //If we did, set the username to the old one
+        socket.emit('chat message', "/nick " + cookie[1]) //I know
     }
 
 
     //The form submission
-    $('form').submit(function(){
+    $('form').submit(function() {
         socket.emit('chat message', $('#m').val());
         $('#m').val('');
         return false;
     });
 
     //Message comes in and we turn it to JSON and parse
-    socket.on('chat message', function(msg){
+    socket.on('chat message', function(msg) {
         var messageObj = JSON.parse(msg);
         //Bold if it came from you
-        if (username === messageObj.uname){
+        if (username === messageObj.uname) {
             $('#messages').append("<li><b>" + messageObj.timestamp +
                 " <span style=\"color: " + messageObj.color + ";\">" +
                 messageObj.uname + "</span>: " +
                 messageObj.message + "</b></li>");
         }
         //Normal if it didn't
-        else{
+        else {
             console.log(messageObj.color);
             $('#messages').append("<li>" + messageObj.timestamp +
                 " <span style=\"color: " + messageObj.color + ";\">" +
@@ -44,48 +43,48 @@ $(function () {
     });
 
     //When we refresh the list of users
-    socket.on('users update', function(userList){
+    socket.on('users update', function(userList) {
         console.log(userList);
-        var userArray=userList.split(",");
+        var userArray = userList.split(",");
         $('#users').html(makeUL(userArray));
     })
 
     //Person left message
-    socket.on('user left', function(lostPerson){
-        $('#messages').append($("<li><i>" + lostPerson +" has disconnected"+"</i></li>"));
+    socket.on('user left', function(lostPerson) {
+        $('#messages').append($("<li><i>" + lostPerson + " has disconnected" + "</i></li>"));
     });
 
     //New person message
-    socket.on('user connect', function(newPerson){
-        $('#messages').append($("<li><i>" + newPerson +" has joined the chat"+"</i></li>"));
+    socket.on('user connect', function(newPerson) {
+        $('#messages').append($("<li><i>" + newPerson + " has joined the chat" + "</i></li>"));
     });
 
-    socket.on('username change', function(changeString){
+    socket.on('username change', function(changeString) {
         var names = changeString.split(",");
-        $('#messages').append($("<li><i>" + names[0] +" has changed their name to "+names[1]+"</i></li>"));
+        $('#messages').append($("<li><i>" + names[0] + " has changed their name to " + names[1] + "</i></li>"));
     });
 
     //set their name
-    socket.on('get name', function(name){
+    socket.on('get name', function(name) {
         $("#username").text(name);
-        username=name;
+        username = name;
         document.cookie = "username=" + name + ";";
-      //  console.log(document.cookie);
+        //  console.log(document.cookie);
     });
     //Set the user color
-    socket.on('get color', function(newcolor){
+    socket.on('get color', function(newcolor) {
         //$("#username").css("color", newcolor); Can't get this to work
-        color=newcolor;
+        color = newcolor;
     });
 
     //Goes through last 200 messages and displays it
-    socket.on('set messages', function(messagingHistory){
-        messagingHistory.forEach(function (msg) {
+    socket.on('set messages', function(messagingHistory) {
+        messagingHistory.forEach(function(msg) {
             var messageObj = JSON.parse(msg);
-                $('#messages').append("<li>" + messageObj.timestamp +
-                    " <span style=\"color: " + messageObj.color + ";\">" +
-                    messageObj.uname + "</span>: " +
-                    messageObj.message + "</li>");
+            $('#messages').append("<li>" + messageObj.timestamp +
+                " <span style=\"color: " + messageObj.color + ";\">" +
+                messageObj.uname + "</span>: " +
+                messageObj.message + "</li>");
             updateScroll();
 
         })
@@ -96,7 +95,7 @@ $(function () {
 function makeUL(array) {
     // Create the list element:
     var list = document.createElement('ul');
-    for(var i = 0; i < array.length; i++) {
+    for (var i = 0; i < array.length; i++) {
         // Create the list item:
         var item = document.createElement('li');
         // Set its contents:
@@ -108,7 +107,7 @@ function makeUL(array) {
     return list;
 }
 
-function updateScroll(){
+function updateScroll() {
     var element = document.getElementById("messageList");
     element.scrollTop = element.scrollHeight;
 }
--- index.js
+++ index.js
@@ -1,85 +1,86 @@
-
 var express = require("express");
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var people =[];
+var people = [];
 var color = [];
 var messagingHistory = [];
 
 app.use('/Public', express.static(__dirname + '/Public'));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/Public/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/Public/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
     //Operations for when a message comes in
-  socket.on('chat message', function(msg){
-      if (msg.startsWith("/nick ")) {
-          var tempName=people[socket.id];
-          people[socket.id]=msg.substr(6, msg.length - 6).trim();
-          socket.emit('get name', people[socket.id]);
-          UpdatePeople(people);
-          io.emit('username change', tempName+","+people[socket.id]);
-      }
-      else if (msg.startsWith("/nickcolor ")) {
-          color[socket.id]=msg.substr(11, msg.length - 11).trim();
-          socket.emit('get color', people[socket.id]);
-      }
-      else{
-          console.log(msg);
-          var date = new Date();
-          //Create a JSON object of the info
-          var messageObj='{'+'' +
-              '"uname":"'+people[socket.id]+'", ' +
-              '"message":"'+msg+'",' +
-              '"timestamp":"'+FormatTime(date)+'",'+
-              '"color":"'+color[socket.id]+
-              '"}';
-          console.log(messageObj);
-          //Emit that object
-          io.emit('chat message', messageObj);
-          //Save to memory BUT only the 200 most recent
-          if (messagingHistory.length > 200){
-              messagingHistory.shift();
-          }
-          messagingHistory.push(messageObj);}
+    socket.on('chat message', function(msg) {
+        if (msg.startsWith("/nick ")) {
+            var tempName = people[socket.id];
+            people[socket.id] = msg.substr(6, msg.length - 6).trim();
+            socket.emit('get name', people[socket.id]);
+            UpdatePeople(people);
+            io.emit('username change', tempName + "," + people[socket.id]);
+        } else if (msg.startsWith("/nickcolor ")) {
+            color[socket.id] = msg.substr(11, msg.length - 11).trim();
+            socket.emit('get color', people[socket.id]);
+        } else {
+            console.log(msg);
+            var date = new Date();
+            //Create a JSON object of the info
+            var messageObj = '{' + '' +
+                '"uname":"' + people[socket.id] + '", ' +
+                '"message":"' + msg + '",' +
+                '"timestamp":"' + FormatTime(date) + '",' +
+                '"color":"' + color[socket.id] +
+                '"}';
+            console.log(messageObj);
+            //Emit that object
+            io.emit('chat message', messageObj);
+            //Save to memory BUT only the 200 most recent
+            if (messagingHistory.length > 200) {
+                messagingHistory.shift();
+            }
+            messagingHistory.push(messageObj);
+        }
 
-  });
-//Create names and join up
-  socket.on('join', function(){
-      console.log("user joined");
-      people[socket.id]="User"+parseInt(Object.keys(people).length+1);
-      color[socket.id]="#000000";
-      socket.emit('get name', people[socket.id]);
-      socket.emit('set messages', messagingHistory);
-      io.emit('user connect', people[socket.id]);
-      UpdatePeople(people);
+    });
+    //Create names and join up
+    socket.on('join', function() {
+        console.log("user joined");
+        people[socket.id] = "User" + parseInt(Object.keys(people).length + 1);
+        color[socket.id] = "#000000";
+        socket.emit('get name', people[socket.id]);
+        socket.emit('set messages', messagingHistory);
+        io.emit('user connect', people[socket.id]);
+        UpdatePeople(people);
     });
 
-//Disconnect message, delete person and users
-  socket.on('disconnect', function(){
-      console.log('user disconnected');
-      io.emit('user left', people[socket.id]);
-      delete people[socket.id];
-      delete color[socket.id];
-      UpdatePeople(people);
+    //Disconnect message, delete person and users
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+        io.emit('user left', people[socket.id]);
+        delete people[socket.id];
+        delete color[socket.id];
+        UpdatePeople(people);
     });
 
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+    console.log('listening on *:' + port);
 });
 
 //A list of all people in the chat
-function UpdatePeople(people){
-    var people_emit="";
-    for(i in people) {
-        if (people_emit===""){people_emit=people[i];}
-        else{people_emit=people_emit+","+people[i];}
+function UpdatePeople(people) {
+    var people_emit = "";
+    for (i in people) {
+        if (people_emit === "") {
+            people_emit = people[i];
+        } else {
+            people_emit = people_emit + "," + people[i];
+        }
     }
     io.emit('users update', people_emit);
 
@@ -94,4 +95,3 @@ function FormatTime(date) {
         minutes = "0" + minutes;
     return hour + ":" + minutes;
 }
-
